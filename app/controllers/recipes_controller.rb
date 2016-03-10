f69a721115cf830c7b8c7ca8e7d64712a34d6a5a
trikitrok <trikitrok@gmail.com>
2016-03-10 20:59:52 +0100

    Rendering view with recipes from Foodd2Fork

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @search_keyword = "chicken"
-    p Recipe.for(@search_keyword)
+    @search_keyword = params[:search] || "chocolate"
+    @recipes = view_context.extract_fields(Recipe.for(@search_keyword))
   end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+  def extract_fields recipes
+    recipes.map do |recipe|
+      {
+        thumbnail: recipe["image_url"],
+        title: recipe["title"],
+        social_rank: recipe["social_rank"]
+      }
+    end
+  end
 end
