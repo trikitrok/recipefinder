86eb9487b5d4b40255e3abb14a44e87bb558b3e5
trikitrok <trikitrok@gmail.com>
2016-03-10 19:05:13 +0100

    Added Recipes model

---
+++ app/models/recipe.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+
+class Recipe
+  include HTTParty
+
+  key_value = ENV[’FOOD2FORK_KEY’]
+  hostport = ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+
+  def for keyword
+
+  end
+end
