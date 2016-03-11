6106d7ed7d472b761e9175dfcc25960f1cdabbae
trikitrok <trikitrok@gmail.com>
2016-03-11 14:03:40 +0100

    Added links to recipes to the recipes thumbnail and title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
     @search_keyword = params[:search] || "chocolate"
-    @recipes = view_context.extract_fields(Recipe.for(@search_keyword))
+    @recipes = Recipe.for(@search_keyword)
   end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,10 +1,11 @@
 module RecipesHelper
-  def extract_fields recipes
+  def extract_recipes_data recipes
     recipes.map do |recipe|
       {
         thumbnail: recipe["image_url"],
         title: recipe["title"],
-        social_rank: recipe["social_rank"]
+        social_rank: recipe["social_rank"],
+        url: recipe["source_url"]
       }
     end
   end
